2755391daf302b7af1935882422e05ceb69740d0
monteiroluana <luanamonteiropereira95@gmail.com>
2021-08-06 17:19:48 -0300

    map

---
+++ export.py
@@ -0,0 +1,42 @@
+import io
+import folium
+import tempfile
+
+from PIL import Image
+from shapely.geometry import Point, mapping, shape
+
+with tempfile.TemporaryDirectory(dir="/tmp") as tempdir:
+    map_path = "{}/map.html".format(tempdir)
+    image_path = "{}/image.png".format(tempdir)
+  
+    # Create map
+    map = folium.Map([-23.682567,-46.666548], zoom_start=14, tiles="cartodbpositron")
+  
+    # Marker my location
+    folium.Marker([-23.682567,-46.666548] ).add_to(map)
+
+    focos = [
+      [ -46.636548, -23.682567 ],
+      [ -46.666548, -23.712567 ],
+      [ -46.696548, -23.682567 ],
+      [ -46.666548, -23.652567 ],
+    ]
+
+    for f in focos:
+        folium.Marker(
+            location=[f[1], f[0]],
+            icon=folium.Icon(color="red",icon="fire"),
+        ).add_to(map)
+
+    # Buffer Location area
+    patch = Point(-46.666548, -23.682567).buffer(0.02)
+    geojson = mapping(patch)
+    folium.GeoJson(geojson).add_to(map)
+   
+    # Save map
+    map.save("map_path.html")
+
+    # # Convert map to PNG
+    # img_data = map._to_png(5)
+    # img = Image.open(io.BytesIO(img_data))
+    # img.save(image_path)
